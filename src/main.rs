788eebe6a7f7fb7f18d55f9d60d571c37af981dd
syukri21 <syukrihsb148@gmail.com>
2024-03-24 15:18:39 +0700

    add router

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router}; // 0.6.1Router;
+mod routes;
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/signup", get(routes::signup::get))
+        .route("/signin", get(routes::signin::get));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
---
+++ src/routes/signin.rs
@@ -0,0 +1,3 @@
+pub async fn get() -> String {
+    return String::from("signin");
+}
