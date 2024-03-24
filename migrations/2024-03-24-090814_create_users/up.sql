f267062ffcd4ed77e25f052199dc6da8245a85f9
syukri21 <syukrihsb148@gmail.com>
2024-03-24 22:21:29 +0700

    add migration and schema

---
+++ migrations/2024-03-24-090814_create_users/up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  deleted_at TIMESTAMP,
+  verified_at TIMESTAMP
+); 
---
+++ src/schema.rs
@@ -0,0 +1,17 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    users (id) {
+        id -> Int4,
+        #[max_length = 255]
+        name -> Varchar,
+        #[max_length = 255]
+        email -> Varchar,
+        #[max_length = 255]
+        password -> Varchar,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+        deleted_at -> Nullable<Timestamp>,
+        verified_at -> Nullable<Timestamp>,
+    }
+}
